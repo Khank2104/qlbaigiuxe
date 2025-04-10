0dad3dc26ea8fb97f51b5a5011596f2241147e10
Khank2104 <nnkhanh159@gmail.com>
2025-04-10 10:15:23 +0700

    code class user gom cac thuoc tinh nhu thiet ke

---
+++ ParkingLot.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace qlbaigiuxe
+{
+    public class ParkingLot
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
+        public string Address { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Capacity must be non-negative")]
+        public int Capacity { get; set; }
+
+    }
+}
