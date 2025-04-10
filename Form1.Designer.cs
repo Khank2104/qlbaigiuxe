8d358679cdc8a460818fdbc093c27ab48d3c7a83
Khank2104 <nnkhanh159@gmail.com>
2025-04-10 10:21:10 +0700

    code cho giao dien

---
+++ Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace qlbaigiuxe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtCapacity = new System.Windows.Forms.TextBox();
+            this.btnAddParkingLot = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(175, 171);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tên bãi đỗ xe";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(201, 205);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "địa chỉ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(201, 251);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "sức chứa";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(312, 171);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(159, 22);
+            this.txtName.TabIndex = 3;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(312, 205);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(159, 22);
+            this.txtAddress.TabIndex = 4;
+            // 
+            // txtCapacity
+            // 
+            this.txtCapacity.Location = new System.Drawing.Point(312, 248);
+            this.txtCapacity.Name = "txtCapacity";
+            this.txtCapacity.Size = new System.Drawing.Size(159, 22);
+            this.txtCapacity.TabIndex = 5;
+            // 
+            // btnAddParkingLot
+            // 
+            this.btnAddParkingLot.Location = new System.Drawing.Point(336, 294);
+            this.btnAddParkingLot.Name = "btnAddParkingLot";
+            this.btnAddParkingLot.Size = new System.Drawing.Size(135, 23);
+            this.btnAddParkingLot.TabIndex = 6;
+            this.btnAddParkingLot.Text = "Thêm bãi đỗ";
+            this.btnAddParkingLot.UseVisualStyleBackColor = true;
+            this.btnAddParkingLot.Click += new System.EventHandler(this.btnAddParkingLot_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.ForeColor = System.Drawing.Color.Red;
+            this.lblMessage.Location = new System.Drawing.Point(309, 346);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(0, 16);
+            this.lblMessage.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(289, 116);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Thêm Bãi Đỗ Xe";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(732, 489);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.btnAddParkingLot);
+            this.Controls.Add(this.txtCapacity);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Thêm Bãi Đỗ Xe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtCapacity;
+        private System.Windows.Forms.Button btnAddParkingLot;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace qlbaigiuxe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddParkingLot_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text.Trim();
+            string address = txtAddress.Text.Trim();
+            string capacityText = txtCapacity.Text.Trim();
+
+            if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(address) || string.IsNullOrEmpty(capacityText))
+            {
+                lblMessage.Text = "Vui lòng nhập đầy đủ Tên, Địa chỉ và Sức chứa!";
+                lblMessage.ForeColor = Color.Red;
+                return;
+            }
+
+            if (!int.TryParse(capacityText, out int capacity) || capacity <= 0)
+            {
+                lblMessage.Text = "Sức chứa phải là số nguyên dương!";
+                lblMessage.ForeColor = Color.Red;
+                return;
+            }
+
+            using (var context = new ParkingLotContext())
+            {
+                // Kiểm tra tên có bị trùng không (bỏ qua hoa thường)
+                bool isNameDuplicate = context.ParkingLots
+                                        .Any(p => p.Name.ToLower() == name.ToLower());
+
+                // Kiểm tra địa chỉ có bị trùng không
+                bool isAddressDuplicate = context.ParkingLots
+                                           .Any(p => p.Address.ToLower() == address.ToLower());
+
+                // Hiển thị thông báo phù hợp
+                if (isNameDuplicate && isAddressDuplicate)
+                {
+                    lblMessage.Text = "Tên và Địa chỉ đã tồn tại!";
+                    lblMessage.ForeColor = Color.Red;
+                    return;
+                }
+                else if (isNameDuplicate)
+                {
+                    lblMessage.Text = "Tên bãi đỗ đã tồn tại!";
+                    lblMessage.ForeColor = Color.Red;
+                    return;
+                }
+                else if (isAddressDuplicate)
+                {
+                    lblMessage.Text = "Địa chỉ bãi đỗ đã tồn tại!";
+                    lblMessage.ForeColor = Color.Red;
+                    return;
+                }
+
+                // Nếu không bị trùng thì thêm mới
+                var parkingLot = new ParkingLot
+                {
+                    Name = name,
+                    Address = address,
+                    Capacity = capacity
+                };
+
+                context.ParkingLots.Add(parkingLot);
+                context.SaveChanges();
+
+                lblMessage.Text = "Thêm bãi đỗ thành công!";
+                lblMessage.ForeColor = Color.Green;
+
+                txtName.Clear();
+                txtAddress.Clear();
+                txtCapacity.Clear();
+
+                var timer = new System.Windows.Forms.Timer();
+                timer.Interval = 3000;
+                timer.Tick += (s, args) =>
+                {
+                    lblMessage.Text = "";
+                    timer.Stop();
+                };
+                timer.Start();
+            }
+        }
+
+
+    }
+}
