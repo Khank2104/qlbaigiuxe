9357d2171acef3e73a4db9cf84affa852f74a03e
Khank2104 <nnkhanh159@gmail.com>
2025-04-10 10:18:17 +0700

    chay code de entity de blingding ra bang csdl

---
+++ ParkingLotContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace qlbaigiuxe
+{
+    public class ParkingLotContext : DbContext
+    {
+        public ParkingLotContext() : base("name=ParkingLotDb") { }
+        public DbSet<ParkingLot> ParkingLots { get; set; }
+    }
+}
